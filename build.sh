d11f9f1e1b7041aec6b105689ddd01ea6522d684
Piyush <piyush.hole2001@gmail.com>
2024-08-02 00:49:01 +0530

    changes done

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Install cmake
-pip install cmake==3.29.3
-pip install face-recognition==1.3.0
-pip install face-recognition-models==0.3.0
-pip install dlib==19.19.0
+pip install cmake
+pip install face-recognition
+pip install face-recognition-models
+pip install dlib
 # Install Python packages
 pip install -r requirements.txt
