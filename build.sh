d5022ec7120d9e5887704dcd92667445468e5a0c
Piyush <piyush.hole2001@gmail.com>
2024-08-02 01:14:09 +0530

    changes done

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 # Install cmake
 pip install cmake
-pip install ./tarballs/dlib-19.24.1-cp311-cp311-win_amd64.whl
+pip install https://files.pythonhosted.org/packages/24/ea/81e4fc5b978277899b1c1a63ff358f1f645f9369e59d9b5d9cc1d57c007c/dlib-19.5.1-cp36-cp36m-win_amd64.whl#sha256=7739535b76eb40cbcf49ba98d894894d06ee0b6e8f18a25fef2ab302fd5401c7
 # Install Python packages
 pip install -r requirements.txt
