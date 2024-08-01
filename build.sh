9ce005ade2b245e204b02ae6765bf0f99fab268f
Piyush <piyush.hole2001@gmail.com>
2024-08-02 01:16:42 +0530

    changes done

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 # Install cmake
-pip install cmake
-pip install https://files.pythonhosted.org/packages/24/ea/81e4fc5b978277899b1c1a63ff358f1f645f9369e59d9b5d9cc1d57c007c/dlib-19.5.1-cp36-cp36m-win_amd64.whl#sha256=7739535b76eb40cbcf49ba98d894894d06ee0b6e8f18a25fef2ab302fd5401c7
+sudo apt-get install cmake
+
+wget https://files.pythonhosted.org/packages/05/57/e8a8caa3c89a27f80bc78da39c423e2553f482a3705adc619176a3a24b36/dlib-19.17.0.tar.gz
+
+tar -xvzf dlib-19.17.0.tar.gz
+
+cd dlib-19.17.0/
+
+sudo python3 setup.py install
 # Install Python packages
 pip install -r requirements.txt
