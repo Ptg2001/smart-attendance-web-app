413cd3a77495ec41a874964cc23673d27ab7e069
Piyush <piyush.hole2001@gmail.com>
2024-08-02 01:07:46 +0530

    changes done

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 # Install cmake
 pip install cmake
-pip install ./tarballs/dlib-19.24.5.tar.gz
+pip install ./tarballs/dlib-19.19.0-cp38-cp38-win_amd64.whl
 # Install Python packages
 pip install -r requirements.txt
