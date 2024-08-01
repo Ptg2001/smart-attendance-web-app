7622e106f0cbfa1f18bb86ccddda38df35cb2458
Piyush <piyush.hole2001@gmail.com>
2024-08-02 01:27:40 +0530

    changes done

--- build.sh
+++ build.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 # Install cmake
-sudo apt-get install cmake
-
-wget https://files.pythonhosted.org/packages/05/57/e8a8caa3c89a27f80bc78da39c423e2553f482a3705adc619176a3a24b36/dlib-19.17.0.tar.gz
-
-tar -xvzf dlib-19.17.0.tar.gz
-
-cd dlib-19.17.0/
-
-sudo python3 setup.py install
+pip install cmake
+pip install dlib-bin
 # Install Python packages
 pip install -r requirements.txt
